757c10c597f25940b385017c195977a1f070aac7
kkj2010 <kunjukim0210@gmail.com>
2022-12-07 13:41:19 -0500

    added categories in nav bar and displays

--- frontend/src/components/homepage/index.jsx
+++ frontend/src/components/homepage/index.jsx
@@ -1,7 +1,115 @@
 import "./index.css";
-import { BiRightArrowAlt } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [panelId, setPanelId] = useState("farmhouse");
+
+  const generatePanel = (panelId) => {
+    let panel = null;
+    switch (panelId) {
+      case "farmhouse":
+        panel = (
+          <div className="tabPanel">
+            <div className="item1">
+              <img src="/img/item1.png" />
+            </div>
+            <div className="item2">
+              <img src="/img/item2.png" />
+            </div>
+            <div className="item3">
+              <img src="/img/item3.png" />
+            </div>
+            <div className="item4">
+              <img src="/img/item4.png" />
+            </div>
+            <div className="item2">
+              <img src="/img/item5.png" />
+            </div>
+            <div className="item3">
+              <img src="/img/item6.png" />
+            </div>
+          </div>
+        );
+        return panel;
+      case "eclectic":
+        panel = (
+          <div className="tabPanel2">
+            <div className="item5">
+              <img src="/img/item10.png" />
+            </div>
+            <div className="item6">
+              <img src="/img/item8.png" />
+            </div>
+            <div className="item7">
+              <img src="/img/item7.png" />
+            </div>
+            <div className="item8">
+              <img src="/img/item11.png" />
+            </div>
+            <div className="item6">
+              <img src="/img/item9.png" />
+            </div>
+            <div className="item7">
+              <img src="/img/item12.png" />
+            </div>
+          </div>
+        );
+        return panel;
+      case "minimal":
+        panel = (
+          <div className="tabPanel1">
+            <div className="item9">
+              <img src="/img/item13.png" />
+            </div>
+            <div className="item10">
+              <img src="/img/item14.png" />
+            </div>
+            <div className="item11">
+              <img src="/img/item16.png" />
+            </div>
+            <div className="item12">
+              <img src="/img/item15.png" />
+            </div>
+            <div className="item10">
+              <img src="/img/item17.png" />
+            </div>
+            <div className="item11">
+              <img src="/img/item18.png" />
+            </div>
+          </div>
+        );
+        return panel;
+      case "holiday":
+        panel = (
+          <div className="tabPanel3">
+            <div className="item13">
+              <img src="/img/item19.png" />
+            </div>
+            <div className="item14">
+              <img src="/img/item20.png" />
+            </div>
+            <div className="item15">
+              <img src="/img/item21.png" />
+            </div>
+            <div className="item16">
+              <img src="/img/item22.png" />
+            </div>
+            <div className="item14">
+              <img src="/img/item23.png" />
+            </div>
+            <div className="item15">
+              <img src="/img/item24.png" />
+            </div>
+          </div>
+        );
+        return panel;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="homepage">
       <div className="textContainer">
@@ -107,7 +215,6 @@ export default function HomePage() {
         </ul>
       </div>
 
-      
       <div className="discoverItemsContainer">
         <div className="discoverItemsTitle">
           <h1>
@@ -116,21 +223,35 @@ export default function HomePage() {
           </h1>
           <div className="tabContainer">
             <ul className="tabLists">
-              <li>Modern farmhouse</li>
-              <li>Boho Decor</li>
-              <li>Eclectic Decor</li>
-              <li>Minimalist</li>
+              <li onClick={() => setPanelId("farmhouse")}>Modern farmhouse</li>
+              <li onClick={() => setPanelId("eclectic")}>Eclectic Decor</li>
+              <li onClick={() => setPanelId("minimal")}>Minimalist</li>
+              <li onClick={() => setPanelId("holiday")}>Holiday Decor</li>
             </ul>
           </div>
-          <div className="tabPanel">
-            <div className="item1"><img src="/img/item1.png"/></div>
-            <div className="item2"><img src="/img/item2.png"/></div>
-            <div className="item3"><img src="/img/item3.png"/></div>
-            <div className="item1"><img src="/img/item4.png"/></div>
-            <div className="item2"><img src="/img/item5.png"/></div>
-            <div className="item3"><img src="/img/item6.png"/></div>
-          </div>
+          {generatePanel(panelId)}
         </div>
+      </div>
+      <div className="footer">
+        <ul className="leftFooter">
+          <li className="flag">
+            <img src="/img/usaFlag.png" />
+          </li>
+          <li> United States</li>
+          <li>l</li>
+          <li>English (US)</li>
+          <li>l</li>
+          <li> $ (USD)</li>
+        </ul>
+        <ul className="rightFooter">
+          <li>&#169; 2022 Shoppy, Inc.</li>
+          <li className="gitHub">
+            <FaGithub />
+          </li>
+          <li className="linkedin">
+            <FaLinkedin />
+          </li>
+        </ul>
       </div>
     </div>
   );
