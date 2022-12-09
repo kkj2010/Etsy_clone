d5624b557fa2b9ebc8f5e7c3143f09da5cae544f
kkj2010 <kunjukim0210@gmail.com>
2022-12-09 17:28:29 -0500

    added carousel

--- frontend/src/components/Products/ProductShow.jsx
+++ frontend/src/components/Products/ProductShow.jsx
@@ -19,7 +19,7 @@ function formatPrice(price) {
 export default function ProductShow() {
   const dispatch = useDispatch();
   const { category, productId } = useParams();
-  const product = useSelector((state) => state.products[productId]);
+  // const product = useSelector((state) => state.products[productId]);
   const [showDescription, setShowDescription] = useState(false);
   const [showShipping, setShowShipping] = useState(false);
 
@@ -29,11 +29,61 @@ export default function ProductShow() {
     }
   }, [dispatch, productId]);
 
+  const product = {
+    id: 1,
+    name: "Sleek Steel Bench",
+    price: 10000,
+    description:
+      "Sometimes people jot down pseudo-code on paper. If that pseudo-code runs directly on their computers, its best, isn't it? Ruby tries to be like that, like pseudo-code that runs. Python people say that too.",
+    seller: {
+      id: 1,
+      email: "user@gmail.com",
+      firstName: "user",
+      createdAt: "2022-12-08T15:57:02.121Z",
+    },
+    category: { id: 3, name: "clothing_shoes", label: "Clothing \u0026 Shoes" },
+    images: [
+      "/img/necklace.png",
+      "/img/necklace2.png",
+      "/img/necklace3.png",
+      "/img/necklace4.png",
+      "/img/necklace5.png",
+      "/img/necklace6.png",
+    ],
+  };
+  console.log(product);
+
   const options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   const [selected, setSelected] = useState(options && options[0]);
   const handleSelect = (e) => setSelected(e.target.value);
   const handleClick = (e) => {
     //move to cart
+  };
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleSwitchImage = (index) => {
+    setCurrentImageIndex(index);
+  };
+
+  const handlePrev = () => {
+    let index;
+    if (currentImageIndex === 0) {
+      index = product.images.length - 1;
+    } else {
+      index = currentImageIndex - 1;
+    }
+    setCurrentImageIndex(index);
+  };
+  const handleNext = () => {
+    let index;
+    if (currentImageIndex === product.images.length - 1) {
+      index = 0;
+    } else {
+      index = currentImageIndex + 1;
+    }
+
+    setCurrentImageIndex(index);
   };
 
   if (!product) {
@@ -47,44 +97,70 @@ export default function ProductShow() {
           <div className="listingPageImage">
             <div className="carouselItems">
               <ul className="carouselItemsList">
-                <li className="carouselItem1">
+                <li
+                  className="carouselItem1"
+                  role="button"
+                  onClick={() => handleSwitchImage(0)}
+                >
                   <img src="/img/necklace.png" />
                 </li>
-                <li className="carouselItem">
+                <li
+                  className="carouselItem"
+                  role="button"
+                  onClick={() => handleSwitchImage(1)}
+                >
                   <img src="/img/necklace2.png" />
                 </li>
-                <li className="carouselItem">
+                <li
+                  className="carouselItem"
+                  role="button"
+                  onClick={() => handleSwitchImage(2)}
+                >
                   <img src="/img/necklace3.png" />
                 </li>
-                <li className="carouselItem">
+                <li
+                  className="carouselItem"
+                  role="button"
+                  onClick={() => handleSwitchImage(3)}
+                >
                   <img src="/img/necklace4.png" />
                 </li>
-                <li className="carouselItem">
+                <li
+                  className="carouselItem"
+                  role="button"
+                  onClick={() => handleSwitchImage(4)}
+                >
                   <img src="/img/necklace5.png" />
                 </li>
-                <li className="carouselItem2">
+                <li
+                  className="carouselItem2"
+                  role="button"
+                  onClick={() => handleSwitchImage(5)}
+                >
                   <img src="/img/necklace6.png" />
                 </li>
               </ul>
             </div>
 
             <div className="innerContainer">
-              <button className="arrowButton">
+              <button className="arrowButton" onClick={handlePrev}>
                 <span className="navArrow">
                   <IoIosArrowBack />
                 </span>
               </button>
 
               <div className="imagesContainer">
-                <img src="/img/necklace.png" />
+                <img src={product.images[currentImageIndex]} />
               </div>
-              <button className="arrowButton">
+
+              <button className="arrowButton" onClick={handleNext}>
                 <span className="navArrow">
                   <IoIosArrowForward />
                 </span>
               </button>
             </div>
           </div>
+
           <div className="rightContainer">
             <h2 className="itemName">{product.name}</h2>
             <p className="itemPrice">{formatPrice(product.price)}</p>
@@ -116,9 +192,9 @@ export default function ProductShow() {
                 >
                   <div className="descriptionTab">
                     <span className="descriptionTitle">Description</span>
-                  <span className="arrowDownButton">
-                    <IoIosArrowDown />
-                  </span>
+                    <span className="arrowDownButton">
+                      <IoIosArrowDown />
+                    </span>
                   </div>
                 </button>
 
@@ -132,10 +208,12 @@ export default function ProductShow() {
                   className="shipping"
                 >
                   <div className="shippingTab">
-                  <span className="shippingTitle">Shipping and return policies</span>
-                  <span className="arrowDownButton">
-                    <IoIosArrowDown />
-                  </span>
+                    <span className="shippingTitle">
+                      Shipping and return policies
+                    </span>
+                    <span className="arrowDownButton">
+                      <IoIosArrowDown />
+                    </span>
                   </div>
                 </button>
               </div>
@@ -148,7 +226,7 @@ export default function ProductShow() {
                   </div>
                   <div className="returnDuration">
                     <p className="returnText">Returns & exchnages window</p>
-                    <p className="returnText2" >30 days</p>
+                    <p className="returnText2">30 days</p>
                   </div>
                 </div>
               )}
